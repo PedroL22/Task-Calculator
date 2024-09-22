3880fe90b23188719349a5e65eae15e39231fbbb
Pedro Lucena <pedrocontact22@gmail.com>
2024-09-22 01:24:43 -0300

    ✨ feat: Add time calculation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import { AddTaskButton } from '~/components/AddTaskButton'
 import { ExportButton } from '~/components/ExportButton'
@@ -9,50 +9,60 @@ import { generateRandomID } from '~/utils/generateRandomID'
 import type { TaskEntity } from '~/entities/TaskEntity'
 
 export const App = () => {
+  const [hoursToWork, _] = useState(8)
   const [remainingTime, setRemainingTime] = useState(8)
   const [tasks, setTasks] = useState<TaskEntity[]>([
     { id: generateRandomID(), code: '', percentage: undefined, time: undefined },
   ])
 
   const addTask = () => {
-    setTasks([...tasks, { id: generateRandomID(), code: '', percentage: undefined, time: undefined }])
+    setTasks((prevTasks) => [
+      ...prevTasks,
+      { id: generateRandomID(), code: '', percentage: undefined, time: undefined },
+    ])
   }
 
   const removeTask = (id: string) => {
-    const newTasks = tasks.filter((task) => task.id !== id)
-    setTasks(newTasks)
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id))
   }
 
   const handleExportData = () => {
-    const dataToExport = tasks.map((task) => {
-      return {
-        code: task.code,
-        percentage: task.percentage,
-        time: task.time,
-      }
-    })
-
+    const dataToExport = tasks.map(({ code, percentage, time }) => ({ code, percentage, time }))
     console.log(dataToExport)
   }
 
+  const updateTask = (id: string, key: keyof TaskEntity, value: any) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) => (task.id === id ? { ...task, [key]: key === 'time' ? Number(value) : value } : task))
+    )
+  }
+
+  useEffect(() => {
+    const totalHours = tasks.reduce((acc, task) => acc + (task.time || 0), 0)
+    setRemainingTime(hoursToWork - totalHours)
+  }, [tasks, hoursToWork])
+
   return (
     <main className='mx-auto max-w-screen-lg p-6'>
-      <h1 className='font-medium text-5xl'>🕐Remaining time: {remainingTime}</h1>
+      <div className='space-y-4'>
+        <h1 className='font-medium text-5xl'>⏳Remaining time: {remainingTime}</h1>
+
+        <h2 className='font-medium text-4xl'>🕐Hours to work: {hoursToWork}</h2>
+      </div>
 
       <div className='mt-10 flex flex-col'>
         <div className='mb-6 flex flex-col space-y-4'>
-          {tasks.map((task) => {
-            return (
-              <TaskRow
-                key={task.id}
-                id={task.id}
-                code={task.code}
-                percentage={task.percentage}
-                time={task.time}
-                onClose={() => removeTask(task.id)}
-              />
-            )
-          })}
+          {tasks.map((task) => (
+            <TaskRow
+              key={task.id}
+              id={task.id}
+              code={task.code}
+              percentage={task.percentage}
+              time={task.time}
+              onChange={updateTask}
+              onClose={() => removeTask(task.id)}
+            />
+          ))}
         </div>
 
         <div className='flex items-center space-x-6'>
--- src/components/TaskRow.tsx
+++ src/components/TaskRow.tsx
@@ -3,17 +3,30 @@ import { TaskInput } from './TaskInput'
 import type { TaskEntity } from '~/entities/TaskEntity'
 
 type TaskRowProps = TaskEntity & {
+  onChange: (id: string, key: keyof TaskEntity, value: string | number | undefined) => void
   onClose: () => void
 }
 
 export const TaskRow = (props: TaskRowProps) => {
   return (
-    <div className='flex w-full items-center justify-between space-x-6'>
-      <TaskInput placeholder='Task ID' value={props.code} />
+    <div className='flex w-full items-center justify-between space-x-4'>
+      <TaskInput
+        placeholder='Task ID'
+        value={props.code}
+        onChange={(e) => props.onChange(props.id, 'code', e.target.value)}
+      />
 
-      <TaskInput placeholder='Percentage %' value={props.percentage} />
+      <TaskInput
+        placeholder='Percentage %'
+        value={props.percentage}
+        onChange={(e) => props.onChange(props.id, 'percentage', e.target.value)}
+      />
 
-      <TaskInput placeholder='Time' value={props.time} />
+      <TaskInput
+        placeholder='Time'
+        value={props.time === 0 ? '' : props.time}
+        onChange={(e) => props.onChange(props.id, 'time', e.target.value)}
+      />
 
       <button type='button' className='text-4xl' onClick={props.onClose}>
         ❌
