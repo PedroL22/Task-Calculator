7ce4fbdfebfd37fd94c71f1dd02771ae26fb2853
Pedro Lucena <pedrocontact22@gmail.com>
2025-05-19 16:35:45 -0300

    🔍 Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -9,6 +10,8 @@ import { App } from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Analytics/>
+
     <Toaster richColors />
 
     <App />
